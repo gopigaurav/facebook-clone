13bbc25a55ca80353c65250a145b1d4e9386034d
gopi <gopigaurav9@gmail.com>
2021-05-29 19:02:45 +0530

    Updates

---
+++ precache-manifest.591429748908a7787a0ae7a182960ed1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63990cf1b589661867aae2d7df60abd4",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "2e340a3b9120d573a904",
+    "url": "/facebook-clone/static/css/main.e1a68fa0.chunk.css"
+  },
+  {
+    "revision": "33a65f8c1cdc152206cb",
+    "url": "/facebook-clone/static/js/2.eb0db3f0.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-clone/static/js/2.eb0db3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e340a3b9120d573a904",
+    "url": "/facebook-clone/static/js/main.0e916867.chunk.js"
+  },
+  {
+    "revision": "1e0b565a3587d3062730",
+    "url": "/facebook-clone/static/js/runtime-main.d9aa6d54.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/facebook-clone/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/facebook-clone/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/facebook-clone/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/facebook-clone/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/facebook-clone/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "3f3ed8cc40df541d3951e95ed55b1a53",
+    "url": "/facebook-clone/static/media/css.3f3ed8cc.jpg"
+  },
+  {
+    "revision": "e34aec03db7623a4ef3d91c8561fdbba",
+    "url": "/facebook-clone/static/media/fbLogo.e34aec03.webp"
+  },
+  {
+    "revision": "093d926c2d4dda19143df09e79f93ddb",
+    "url": "/facebook-clone/static/media/fbTextLogo.093d926c.svg"
+  },
+  {
+    "revision": "5e37d697efc51f4cca98dae9e2dcbb92",
+    "url": "/facebook-clone/static/media/front-end-steps.5e37d697.jpg"
+  },
+  {
+    "revision": "fe155e686b98950080fb2b92d0640414",
+    "url": "/facebook-clone/static/media/front-end.fe155e68.jpg"
+  },
+  {
+    "revision": "a586897fe1f8d26e936d8513fd371e04",
+    "url": "/facebook-clone/static/media/html.a586897f.jpg"
+  },
+  {
+    "revision": "87a6e1f3e16c0b6b943d8f901bedc00c",
+    "url": "/facebook-clone/static/media/js.87a6e1f3.jpg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/facebook-clone/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/facebook-clone/static/media/plus.9311af2e.svg"
+  }
+]);
